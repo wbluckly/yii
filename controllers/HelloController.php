0c4fce4891f27123ebdefadb8b40d523af713e37
wb <wblucky@126.com>
2016-06-26 17:31:52 +0800

    响应组件

--- controllers/HelloController.php
+++ controllers/HelloController.php
@@ -30,4 +30,29 @@ class HelloController extends Controller
         //echo "hello world!";
     }
 
+    public function actionResponse()
+    {
+      $res = \YII::$app->response;
+
+      //定义返回状态码
+      //$res->statusCode = '404';
+      //设置响应头
+      //$res->headers->set('pragma', 'no-cache');//不缓存
+      //$res->headers->set('pragma', 'max-age=5');//缓存5秒钟
+      //删除头信息
+      //$res->headers->remove('pragma');//删除pragma头信息
+
+      //跳转
+      //$res->headers->add('Location', 'http://www.baidu.com');//不是很好使
+      //$this->redirect('http://www.baidu.com', 302);//这个好使
+
+      //下载
+      //$res->headers->add('content-disposition', 'attachment; filename="a.jpg"'); 
+      //$res->sendFile('./robots.txt');
+    }
+
+    public function actionSession()
+    {
+      
+    }
 }
